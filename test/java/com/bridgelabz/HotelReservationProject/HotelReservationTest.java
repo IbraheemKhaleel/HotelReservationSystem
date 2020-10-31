59e0d99becae8e92489801abd0c8695e463327d4
IbraheemKhaleel <ikhaleelem@gmail.com>
2020-10-31 20:49:23 +0530

    Added : code for testing the conditions  for customer entries

--- test/java/com/bridgelabz/HotelReservationProject/HotelReservationTest.java
+++ test/java/com/bridgelabz/HotelReservationProject/HotelReservationTest.java
@@ -6,30 +6,30 @@ import org.junit.Test;
 public class HotelReservationTest {
     @Test
     public void givenHotelDetails_WhenAddedForHotel_ShouldReturnEnteredName() {
-        HotelReservation hotelReservation = new HotelReservation();
-        HotelInfos newHotel = hotelReservation.addHotel("Lakewood" , "Normal" , 110 , 90 );
-        Assert.assertEquals(newHotel.hotelName , "Lakewood");
+        HotelReservation hotelReservation = new HotelReservation() ;
+        HotelInfos newHotel = hotelReservation.addHotel("Lakewood" , "Normal" , 110 , 90 ) ;
+        Assert.assertEquals(newHotel.hotelName , "Lakewood") ;
     }
 
     @Test
     public void givenHotelDetails_WhenAddedForHotel_ShouldReturnEnteredCustomerType() {
-        HotelReservation hotelReservation = new HotelReservation();
-        HotelInfos newHotel = hotelReservation.addHotel("Lakewood" , "Normal" , 110 , 90 );
-        Assert.assertEquals(newHotel.typeOfCustomer , "Normal");
+        HotelReservation hotelReservation = new HotelReservation() ;
+        HotelInfos newHotel = hotelReservation.addHotel("Lakewood" , "Normal" , 110 , 90 ) ;
+        Assert.assertEquals(newHotel.typeOfCustomer , "Normal") ;
     }
 
     @Test
     public void givenHotelDetails_WhenAddedForHotel_ShouldReturnEnteredWeekdayRate() {
-        HotelReservation hotelReservation = new HotelReservation();
-        HotelInfos newHotel = hotelReservation.addHotel("Lakewood" , "Normal" , 110 , 90 );
-        Assert.assertEquals(newHotel.weekRate , 110);
+        HotelReservation hotelReservation = new HotelReservation() ;
+        HotelInfos newHotel = hotelReservation.addHotel("Lakewood" , "Normal" , 110 , 90 ) ;
+        Assert.assertEquals(newHotel.weekRate , 110) ;
     }
 
     @Test
     public void givenHotelDetails_WhenAddedForHotel_ShouldReturnEnteredWeekDayRate() {
-        HotelReservation hotelReservation = new HotelReservation();
-        HotelInfos newHotel = hotelReservation.addHotel("Lakewood" , "Normal" , 110 , 90 );
-        Assert.assertEquals(newHotel.weekendRate , 90);
+        HotelReservation hotelReservation = new HotelReservation() ;
+        HotelInfos newHotel = hotelReservation.addHotel("Lakewood" , "Normal" , 110 , 90 ) ;
+        Assert.assertEquals(newHotel.weekendRate , 90) ;
     }
 
 }
