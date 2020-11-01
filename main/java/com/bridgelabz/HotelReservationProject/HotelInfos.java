650e5092849a5f52660f8bf784f29fd7cdb41a49
IbraheemKhaleel <ikhaleelem@gmail.com>
2020-11-01 12:15:45 +0530

    Added : code for creating class for field variables

--- main/java/com/bridgelabz/HotelReservationProject/HotelInfos.java
+++ main/java/com/bridgelabz/HotelReservationProject/HotelInfos.java
@@ -1,15 +1,38 @@
 package com.bridgelabz.HotelReservationProject;
 
 public class HotelInfos {
-    String hotelName;
-    String typeOfCustomer;
-    int weekRate;
-    int weekendRate;
+    private String hotelName;
+    private String typeOfCustomer;
+    private int weekdayRate;
+    public int costWeekday;
 
-    public HotelInfos(String hotelName, String typeOfCustomer, int weekRate, int weekendRate) {
+    public HotelInfos(){
+
+    }
+
+    public HotelInfos(String hotelName, String typeOfCustomer, int weekdayRate) {
         this.hotelName = hotelName;
         this.typeOfCustomer = typeOfCustomer;
-        this.weekRate = weekRate;
-        this.weekendRate = weekendRate;
+        this.weekdayRate = weekdayRate;
+    }
+
+    public String getHotelName() {
+        return hotelName;
+    }
+
+    public String getTypeOfCustomer() {
+        return typeOfCustomer;
+    }
+
+    public int getWeekdayRate() {
+        return weekdayRate;
+    }
+
+    public int getCostWeekday() {
+        return  costWeekday;
+    }
+
+    public void setCostWeekDay(int costWeekday) {
+        this.costWeekday = costWeekday;
     }
 }
