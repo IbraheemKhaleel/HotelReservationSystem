4200a85ffb3c86d47b9e211bdfff9110c8e52f4d
IbraheemKhaleel <ikhaleelem@gmail.com>
2020-11-02 07:25:12 +0530

    Refactored : code for creating method to find the best rated cheapest hotel

--- main/java/com/bridgelabz/HotelReservationProject/HotelReservation.java
+++ main/java/com/bridgelabz/HotelReservationProject/HotelReservation.java
@@ -13,6 +13,7 @@ public class HotelReservation  {
     HotelInfos addHotel(String name , String type , int weekday , int weekend , int rating) {
         return new HotelInfos(name , type , weekday ,weekend, rating);
     }
+    // method to find cheapest hotel in the basis of given date range
     public Result findCheapestHotel(ArrayList<HotelInfos> hotelArray , String dateS , String dateE) {  //gets total cost for each hotel and returns cheapest hotel
 
         Integer cost;
@@ -34,15 +35,30 @@ public class HotelReservation  {
             cost = hotelInfos.getTotalRate(dateStart, dateEnd, difference);
             hotelInfos.setTotalCost(cost);
         }
-        return this.getCheapestHotel(hotelArray);
+        Result result = this.getCheapestHotel(hotelArray);
+        result = this.cheapestBestRatedHotel(hotelArray , result);
+        return result;
     }
-
+    // method to get the cheapest hotel
     public Result getCheapestHotel(ArrayList<HotelInfos> hotelArray){ //compares total costs of hotels
         Optional<HotelInfos> cheapestHotel = hotelArray.stream().min(Comparator.comparingInt(hotel -> hotel.getTotalCost()));
         Result result = new Result();
         result.setHotelName(cheapestHotel.get().getHotelName());
         result.setTotalCost(cheapestHotel.get().getTotalCost());
         System.out.println(result.getHotelName() + result.getTotalCost());
+        return result;
+    }
+    // method to get the cheapest and best rated hotel
+    public Result cheapestBestRatedHotel(ArrayList<HotelInfos> hotelArray , Result result) {
+        Optional<HotelInfos> maxCostHotel = hotelArray.stream().max(Comparator.comparingInt(hotel -> hotel.getTotalCost()));
+        Optional<HotelInfos> minRatingHotel = hotelArray.stream().min(Comparator.comparingInt(hotel -> hotel.getRating()));
+        for (HotelInfos hotel : hotelArray) {
+            if (hotel.getTotalCost() < maxCostHotel.get().getTotalCost() && hotel.getRating() > minRatingHotel.get().getRating()) {
+                result.setRating(hotel.getRating());
+                result.setHotelName(hotel.getHotelName());
+                result.setTotalCost(hotel.getTotalCost());
+            }
+        }
         return result;
     }
 
